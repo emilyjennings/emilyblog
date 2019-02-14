fdb3ac5776b85025f260353ef460c40e4dc6148f
github username <github email address>
2019-02-13 23:34:50 -0500

    styling and making some things work better

--- app/assets/javascripts/cable.js
+++ app/assets/javascripts/cable.js
@@ -11,3 +11,24 @@
   App.cable = ActionCable.createConsumer();
 
 }).call(this);
+
+$(".nav").hide();
+$("#hamburger").click(function(){
+  $(".nav").slideToggle("slow", function(){
+    $("#hamburger").hide();
+    $(".nav").show();
+    $("#cross").show();
+  });
+});
+
+
+$("#cross").click(function(event){
+  event.preventDefault();
+  //click event that takes information from the event and prevents refresh
+  if ($("#cross").is(":visible")){
+    $(".nav").slideToggle("slow", function(){
+      $("#cross").hide();
+      $("#hamburger").show();
+    });
+  }
+});
